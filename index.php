b05117f7271382f50903db0361bd495fa401a317
Tony G <63308979+tsgrewing@users.noreply.github.com>
2023-03-06 14:57:01 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$var1 = 4;
+$var2 = 6;
+$var3 = 
+"<html>
+    <head></head>
+    <body>
+        <h1>
+        Hello from Tony Grewing
+        </h1>
+</html>";
+
+echo $var1 + $var2;
+echo $var3;
+?>
