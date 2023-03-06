05c0597dfa738d66631eca799319b1c5a73a7697
Tony G <63308979+tsgrewing@users.noreply.github.com>
2023-03-06 16:10:27 -0600

    array practice

--- form.php
+++ form.php
@@ -1,19 +1,27 @@
 <?php
 
-if (isset($_GET["name"])){
-    $nameVar = $_GET["name"];
+if (isset($_POST["name"])){
+    $nameVar = $_POST["name"];
 }
 else {
-    $nameVar="Guest";
+    $nameVar='Guest';
 }
+
+$arrVar = array(
+    array('Volvo', 22, 18),
+    array('BMW', 15, 13),
+    array('Saab', 5, 2),
+    array('Land Rover', 17, 15)    
+);
+
 $var3 = 
 "<html>
     <head></head>
     <body>
         <h1>
-        Hello $nameVar, welcome to the site!
+        Hello $nameVar, welcome to  the site!
         </h1>
-        <form action='http://localhost/sqlphp/form.php' method='GET'>
+        <form action='http://localhost/sqlphp/form.php' method='POST'>
             <label for='name'>Enter your name:</label><br>
             <input type='text' id='name' name='name'></input><br>
             <input type='submit' value='submit'>
@@ -22,4 +30,12 @@ $var3 =
 </html>";
 
 echo $var3;
+
+foreach($arrVar as $i) {
+    foreach($i as $val) {
+        echo $val . " ";
+    }
+    echo '<br>';
+};
+
 ?>
